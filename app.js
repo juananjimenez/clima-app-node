b1f7c4675bc52d30af7aa1a8c6d80ddbc5b65959
Juanan Jiménez <juananjimenez1975@gmail.com>
2019-02-25 01:16:16 +0100

    primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const lugar = require('./lugar/lugar');
+const argv = require('yargs').options({
+    direccion: {
+        alias: 'd',
+        desc: 'Dirección de la ciudad para obtener el clima',
+        demand: true
+    }
+}).argv;
+
+lugar.getLugarLatLng(argv.direccion)
+    .then(resp => {
+        console.log(resp);
+    })
+    .catch(e => console.log(e));
